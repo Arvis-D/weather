e8167453aeabe9b86963ce57cc33540115fa7730
DeArviss <arvis.deicmans@chi.lv>
2023-06-04 08:38:01 +0300

    make a request for weather data

--- lib/api/weather_api.dart
+++ lib/api/weather_api.dart
@@ -1,4 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:dio_logging_interceptor/dio_logging_interceptor.dart';
+import 'package:weather/env.dart';
 
 class WeatherApi {
+  static final Dio dio = _createDio();
+  static const String _baseUrl =
+      "https://api.openweathermap.org/data/2.5/forecast";
 
+  static getWeatherData() async {
+    final Response response = await dio.get(
+      _baseUrl,
+      queryParameters: {
+        'appid': Env.weatherApiKey,
+        'lat': '41.9028',
+        'lon': '12.4964'
+      },
+    );
+  }
+
+  static Dio _createDio() {
+    final dio = Dio();
+
+    // Todo: to use this I have to use older dio version so find a better solution
+    dio.interceptors.add(DioLoggingInterceptor(
+      level: Level.body,
+      compact: false,
+    ));
+
+    return dio;
+  }
 }
